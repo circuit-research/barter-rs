945ec80717e249b5b6e5ae4d1928b198f2fd8b5b
JustAStream <keirinvestment@gmail.com>
2022-09-09 20:01:29 +0100

    feat: state machine paths mapped out

--- src/cerebrum/market.rs
+++ src/cerebrum/market.rs
@@ -1,15 +1,16 @@
 use crate::cerebrum::Cerebrum;
-use barter_data::model::{DataKind, MarketEvent};
+use crate::cerebrum::order::Algorithmic;
 use crate::cerebrum::OrderGenerator;
+use barter_data::model::{DataKind, MarketEvent};
 
 /// MarketUpdater can transition to:
-///  a) OrderGenerator
+///  a) OrderGenerator<Algorithmic>
 pub struct MarketUpdater {
     pub market: MarketEvent
 }
 
 impl Cerebrum<MarketUpdater> {
-    fn update(mut self) -> Cerebrum<OrderGenerator> {
+    fn update(mut self) -> Cerebrum<OrderGenerator<Algorithmic>> {
         // Update Positions, Statistics, Indicators
         match self.state.market.kind {
             DataKind::Trade(_) => {
@@ -24,17 +25,18 @@ impl Cerebrum<MarketUpdater> {
     }
 }
 
-/// a) MarketUpdater -> OrderGenerator
-impl From<Cerebrum<MarketUpdater>> for Cerebrum<OrderGenerator> {
+/// a) MarketUpdater -> OrderGenerator<Algorithmic>
+impl From<Cerebrum<MarketUpdater>> for Cerebrum<OrderGenerator<Algorithmic>> {
     fn from(cerebrum: Cerebrum<MarketUpdater>) -> Self {
         Self {
             feed: cerebrum.feed,
+            event_tx: cerebrum.event_tx,
             event_q: cerebrum.event_q,
             balances: cerebrum.balances,
             orders: cerebrum.orders,
             positions: cerebrum.positions,
             strategy: cerebrum.strategy,
-            state: OrderGenerator
+            state: OrderGenerator { state: Algorithmic }
         }
     }
 }
