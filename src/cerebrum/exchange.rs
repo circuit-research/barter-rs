75e63c3aa8043d7cd8939f48945b411687bbdc94
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2022-09-10 11:56:59 +0100

    feat: bits and bobs

--- src/cerebrum/exchange.rs
+++ src/cerebrum/exchange.rs
@@ -1,9 +1,14 @@
-
+use super::{Balance, ClientOrderId, event::ConnectionStatus};
+use barter_integration::model::{Instrument, Market};
+use std::{
+    time::Duration,
+    collections::HashMap
+};
 
 // Todo: May need to have an synchronous interface prior to async for eg/ GenerateClientOrderId
 pub enum ExchangeCommand {
     // Check connection status
-    Status,
+    ConnectionStatus,
 
     // Fetch Account State
     FetchOpenOrders,
@@ -18,4 +23,76 @@ pub enum ExchangeCommand {
     CancelOrderByInstrument,
     CancelOrderByBatch,
     CancelOrderAll,
+}
+
+pub trait ExchangeClient {
+    fn instruments(&self) -> &[Instrument];
+    fn connection_status(&self) -> ConnectionStatus;
+
+    fn fetch_orders_open(&self) -> ();
+    fn fetch_balances(&self) -> ();
+
+    fn open_order(&self) -> ();
+    fn open_order_batch(&self) -> ();
+
+    fn cancel_order_by_id(&self) -> ();
+    fn cancel_order_by_instrument(&self) -> ();
+    fn cancel_order_by_batch(&self) -> ();
+    fn cancel_order_all(&self) -> ();
+}
+
+pub struct SimulatedExchange {
+    pub meta: SimulationMeta,
+    pub latency: Duration,
+    pub instruments: Vec<Instrument>,
+    pub connection_status: ConnectionStatus,
+    pub open: HashMap<ClientOrderId, ()>,
+    pub balances: HashMap<Instrument, Balance>,
+}
+
+pub struct SimulationMeta {
+    fees: (),
+    latency: Duration,
+}
+
+impl ExchangeClient for SimulatedExchange {
+    fn instruments(&self) -> &[Instrument] {
+        &self.instruments
+    }
+
+    fn connection_status(&self) -> ConnectionStatus {
+        self.connection_status
+    }
+
+    fn fetch_orders_open(&self) -> () {
+
+    }
+
+    fn fetch_balances(&self) -> () {
+        todo!()
+    }
+
+    fn open_order(&self) -> () {
+        todo!()
+    }
+
+    fn open_order_batch(&self) -> () {
+        todo!()
+    }
+
+    fn cancel_order_by_id(&self) -> () {
+        todo!()
+    }
+
+    fn cancel_order_by_instrument(&self) -> () {
+        todo!()
+    }
+
+    fn cancel_order_by_batch(&self) -> () {
+        todo!()
+    }
+
+    fn cancel_order_all(&self) -> () {
+        todo!()
+    }
 }
