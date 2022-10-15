482b68675994d3b04818bc2907f62c479f407749
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2022-10-15 20:33:51 +0100

    feat: state initialilser gets it's own mod

---
+++ src/engine/state/initialise.rs
@@ -0,0 +1,63 @@
+use super::{
+    consume::Consume,
+    terminate::Terminate,
+};
+use crate::{
+    engine::{
+        Engine, Trader,
+    },
+    portfolio::{AccountUpdater, Initialiser, MarketUpdater}
+};
+use barter_integration::model::{Exchange, Instrument};
+use std::{
+    collections::HashMap,
+    marker::PhantomData,
+};
+
+/// [`Initialise`] can transition to one of:
+/// a) [`Consumer`]
+/// b) [`Terminate`]
+pub struct Initialise<Portfolio> {
+    pub instruments: HashMap<Exchange, Vec<Instrument>>,
+    pub phantom: PhantomData<Portfolio>,
+}
+
+impl<Strategy, Portfolio> Trader<Strategy, Initialise<Portfolio>>
+where
+    Portfolio: Initialiser<Output = Portfolio> + MarketUpdater + AccountUpdater,
+{
+    pub fn init(self) -> Engine<Strategy, Portfolio> {
+        // De-structure Self to access attributes required for Portfolio Initialiser
+        let Self {
+            mut feed,
+            strategy,
+            execution_tx,
+            state: Initialise { instruments, .. },
+        } = self;
+
+        match Portfolio::init(instruments, &execution_tx, &mut feed) {
+            // a) Initialise -> Consume
+            Ok(portfolio) => {
+                Engine::Consume(Trader {
+                    feed,
+                    strategy,
+                    execution_tx,
+                    state: Consume {
+                        portfolio
+                    }
+                })
+            }
+            // b) Initialise -> Terminate
+            Err(error) => {
+                Engine::Terminate(Trader {
+                    feed,
+                    strategy,
+                    execution_tx,
+                    state: Terminate {
+                        reason: Err(error)
+                    }
+                })
+            }
+        }
+    }
+}
