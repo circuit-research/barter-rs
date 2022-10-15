e4bdb4c59da07fc8d69fd0e6aa798d9dc1e88d82
Just A Stream <93921983+just-a-stream@users.noreply.github.com>
2022-10-15 10:44:02 +0100

    feat: account & market updater boiletplate

--- src/engine/state/account.rs
+++ src/engine/state/account.rs
@@ -1,1 +1,33 @@
+use barter_execution::model::{AccountEvent, AccountEventKind};
+use crate::engine::{Engine, Trader};
+use crate::engine::state::consumer::Consumer;
+
+/// [`AccountUpdater`] can only transition to:
+/// a) [`Consumer`]
 pub struct AccountUpdater;
+
+impl Trader<AccountUpdater> {
+    pub fn update(self, account: AccountEvent) -> Engine {
+        // Todo: Update accounts
+        match account.kind {
+            AccountEventKind::OrdersOpen(_) => {}
+            AccountEventKind::OrdersNew(_) => {}
+            AccountEventKind::OrdersCancelled(_) => {}
+            AccountEventKind::Balance(_) => {}
+            AccountEventKind::Trade(_) => {}
+            AccountEventKind::Balances(_) => {}
+        }
+
+        Engine::Consumer(Trader::from(self))
+    }
+}
+
+/// a) AccountUpdater -> Consumer
+impl From<Trader<AccountUpdater>> for Trader<Consumer> {
+    fn from(trader: Trader<AccountUpdater>) -> Self {
+        Self {
+            state: Consumer,
+            feed: trader.feed
+        }
+    }
+}
